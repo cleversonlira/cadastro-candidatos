ee53da7f5887d0ad17438158de6454ad187f94f7
cleversonlira <cleverson_lira@outlook.com>
2022-01-05 21:21:26 -0300

    Adicionando docker-compose.yml com container do banco e do keycloak.

--- src/main/java/br/com/qwa/domain/Usuario.java
+++ src/main/java/br/com/qwa/domain/Usuario.java
@@ -1,12 +1,10 @@
 package br.com.qwa.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 
 @Entity
+@Table(name = "usuario")
 public class Usuario {
 
     @Id
